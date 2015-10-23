3f1caaeb034f96ac0447200a4d1e1cf1a4f33327
liuchuo <95323362@qq.com>
2015-10-23 20:01:43 +0800

    heheda~

---
+++ 20151023-3/20151023-3/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main() {
+    using namespace std;
+    int hdvsyu = 250;
+    cout << "I hold the view that";
+    cout << endl;
+    cout << "hdvsyu is a  ";
+    cout << hdvsyu;
+    cout <<".lalala~\n";
+    hdvsyu = hdvsyu - 248;
+    cout << "Now,he is so ~~~ " << hdvsyu << "   :)" << endl;
+    return 0;
+}
