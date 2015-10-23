5cfad933afe0857422fd940249c8647064a4d5fa
liuchuo <95323362@qq.com>
2015-10-23 20:10:05 +0800

    ahahahahaha

---
+++ 20151023-4/20151023-4/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main() {
+    using namespace std;
+    cout << "Who is hdvsyu ?" << endl;
+    int hdvsyu;
+    cin >> hdvsyu;
+    cout << "hdvsyu is a " << hdvsyu << " ahahahahaha~~~ :) ";
+    return 0; 
+}
