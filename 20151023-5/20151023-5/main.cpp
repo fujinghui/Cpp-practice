690a36b9be958c80ccd175449403303c2062c385
liuchuo <95323362@qq.com>
2015-10-24 13:24:49 +0800

    using namespace std;

--- 20151023-5/20151023-5/main.cpp
+++ 20151023-5/20151023-5/main.cpp
@@ -1,6 +1,20 @@
+//#include <iostream>
+//#include <cmath>
+//int main() {
+//    std::cout << "输入一个值，我会知道它的平方根是多少哦~~" << std::endl;
+//    double a;
+//    std::cin >> a;
+//    double b = sqrt(a);
+//    std::cout << a << "的平方根是" << b <<".~~~\n";
+//    return 0;
+//}
+
 #include <iostream>
 #include <cmath>
 int main() {
+//    using std::cout;
+//    using std::cin;
+//    using std::endl;
     using namespace std;
     cout << "输入一个值，我会知道它的平方根是多少哦~~" << endl;
     double a;
