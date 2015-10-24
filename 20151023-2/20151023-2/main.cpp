47e54fc9b5de4bd758b1218c81e49539e70e2bba
liuchuo <95323362@qq.com>
2015-10-24 19:43:31 +0800

    hex oct

---
+++ 20151023-2/20151023-2/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <climits>
+int main() {
+    using namespace std;
+    int a = INT_MAX;
+    cout << a << endl;
+    cout << a + 1 << endl;
+    cout << LONG_MAX + 1 << endl;
+    cout << INT_MIN - 1 << endl;
+    a = a + 2;
+    cout << a << endl;
+    int b = 42;
+    cout << "b = " << b << endl;
+    cout << hex;
+    cout << "hex -> b = " << b << endl;
+    cout << oct;
+    cout << "oct -> b = " << b << endl;
+    return 0;
+}
