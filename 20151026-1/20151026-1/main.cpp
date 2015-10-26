1295e4843c4473d7620d2761f1bb6df62eab536e
liuchuo <95323362@qq.com>
2015-10-26 12:33:30 +0800

    cin.get(a,b).get();

--- 20151026-1/20151026-1/main.cpp
+++ 20151026-1/20151026-1/main.cpp
@@ -7,10 +7,16 @@ int main() {
     const int MaxSize = 30;
     char A[MaxSize];
     char B[MaxSize];
+    char C[MaxSize];
+    char D[MaxSize];
     cout << "Hello!~~" << endl;
     cin.getline(A,MaxSize);
     cout << "Fine,thank you~~" << endl;
     cin.getline(B,MaxSize);
     cout << "I am fine,too~~~" << endl;
+    
+    cin.get(C,MaxSize).get();
+    cin.get(D,MaxSize);
+    cout << A << B << C << D << endl;
     return 0;
 }
