c56caeba7101afb63b643d4ceed3cb5286b86aa5
陈欣 <95323362@qq.com>
2015-11-02 20:51:44 +0800

    using new with a structure

---
+++ 20151002-4/20151002-4/main.cpp
@@ -0,0 +1,22 @@
+//using new with a structure
+#include <iostream>
+struct inflatable {
+    char name[20];
+    float volume;
+    double price;
+};
+int main() {
+    using namespace std;
+    inflatable *ps = new inflatable;
+    cout << "enter name of inflatable item: ";
+    cin.get(ps->name,20);
+    cout << "enter volume in cubic feet: ";
+    cin >> (*ps).volume;
+    cout << "enter price:$";
+    cin >> ps->price;
+    cout << "Name: " << (*ps).name << endl;
+    cout << "volume: " << ps->volume << " cubic feet\n";
+    cout << "price: $" << ps->price << endl;
+    delete ps;
+    return 0;
+}
