2079007fe0f4be459b2200c0f0ee151627f86a2c
陈欣 <95323362@qq.com>
2015-11-02 20:18:00 +0800

    使用delete释放内存

--- 20151102-1/20151102-1/main.cpp
+++ 20151102-1/20151102-1/main.cpp
@@ -1,4 +1,5 @@
 //使用new动态分配内存
+//使用delete释放内存
 #include <iostream>
 int main() {
     using namespace std;
@@ -15,5 +16,7 @@ int main() {
     cout << "size of *pt = " << sizeof(*pt) << endl;
     cout << "size of pd = " << sizeof(pd)<< endl;
     cout << "size of *pd = " << sizeof(*pd) << endl;
+    delete pt;//释放pt所指向的内存，而不会删除指针pt本身。一定要配对使用new和delete，否则将发生内存泄露。
+    delete pd;
     return 0;
 }
