0eb211f959dc3642c2b9e898ebdad1c03a0ee086
陈欣 <95323362@qq.com>
2015-11-03 16:10:42 +0800

    cout.setf(ios_base::boolalpha);

---
+++ 20151103-1/20151103-1/main.cpp
@@ -0,0 +1,19 @@
+//cout.setf(ios_base::boolalpha);
+#include <iostream>
+int main() {
+    using namespace std;
+    int x;
+    cout << "The expression x = 100 has the value ";
+    cout << (x = 100) << endl;
+    cout << "Now x = " << x << endl;
+    cout << "the expression x < 3 has the value ";
+    cout << (x < 3) << endl;
+    cout << "the expression x > 3 has the value ";
+    cout << (x > 3) << endl;
+    cout.setf(ios_base::boolalpha);
+    cout << "the expression x < 3 has the value ";
+    cout << (x < 3) << endl;
+    cout << "the expression x > 3 has the value ";
+    cout << (x > 3) << endl;
+    return 0;
+}
