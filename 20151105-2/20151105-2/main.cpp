31d775805814c89c6a5792d8e6b41e4ec5cb8514
陈欣 <95323362@qq.com>
2015-11-05 22:16:20 +0800

    比较string类字符串

---
+++ 20151105-2/20151105-2/main.cpp
@@ -0,0 +1,13 @@
+//比较string类字符串
+#include <iostream>
+#include <string>
+int main() {
+    using namespace std;
+    string word = "?ate";
+    for (char ch = 'a'; word != "mate"; ch++) {
+        cout << word << endl;
+        word[0] = ch;
+    }
+    cout << "After loop ends, word is " << word << endl;
+    return 0;
+}
