abc5dccb0f83953417785054950fccf53ed5d445
陈欣 <95323362@qq.com>
2015-11-08 13:57:27 +0800

    using structure references

--- 20151107-1/20151107-1/main.cpp
+++ 20151107-1/20151107-1/main.cpp
@@ -35,9 +35,31 @@ int main() {
     display(team);
     cout << "Displaying dup after assignment:\n";
     display(dup);
-    ste_pc(four);
+    set_pc(four);
     accumulate(dup, five) = four;
     cout << "Displaying dup after ill-advised assignment:\n";
     display(dup);
     return 0;
+}
+
+void display(const free_throws &ft) {
+    using std::cout;
+    cout << "Name: " << ft.name << '\n';
+    cout << " Made: " << ft.made << '\t';
+    cout << "Attempts: " << ft.attempts << '\t';
+    cout << "Percent: " << ft.percent << '\n';
+}
+
+void set_pc(free_throws &ft) {
+    if (ft.attempts != 0)
+        ft.percent = 100.0f * float(ft.made) / float(ft.attempts);
+    else
+        ft.percent = 0;
+}
+
+free_throws &accumulate(free_throws &target, const free_throws &source) {
+    target.attempts += source.attempts;
+    target.made += source.made;
+    set_pc(target);
+    return target;
 }
