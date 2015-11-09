fd707c80acbd96aca401f68feba0399127ea93e9
陈欣 <95323362@qq.com>
2015-11-09 20:56:17 +0800

    list<int> one(5, 2);五个二。。

---
+++ 20151109-9/20151109-9/main.cpp
@@ -0,0 +1,14 @@
+//using a list-insert()的用法
+#include <iostream>
+#include <list>
+#include <iterator>
+#include <algorithm>
+
+void outint(int n) {std::cout << n <<" ";}
+
+int main() {
+    using namespace std;
+    list<int> one(5, 2);
+    int stuff[5] = {1, 2, 4, 8, 6};
+    
+}
