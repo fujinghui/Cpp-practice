cdacebce55e38df8c555f34abbf05f7e3d9f9178
陈欣 <95323362@qq.com>
2016-03-08 16:08:51 +0800

    cctype note

---
+++ include-cctype-note.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cctype>
+char c;
+isalnum(c) 是数字或者字母
+isalpha(c) 是字母
+isspace(c) \n \t \r \f \v space
+isblank(c) \t space
+isdigit(c) 数字
+islower(c) 小写字母
+isupper(c) 大写字母
+ispunct(c) 标点符号
+char t = tolower(c); 转换为小写字母
+char t = toupper(c); 转换为大写字母
